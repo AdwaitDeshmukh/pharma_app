dd0c3a496febc5600118f562e953b22ad9efc080
AdwaitDeshmukh <adwaitdeshmukh1121@gmail.com>
2025-11-21 00:10:01 +0530

    login page ready with authentication

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
-from utils.db import init_db
+from flask import Flask, request, redirect, render_template
+from utils.db import init_db, get_db
 
 app = Flask(__name__)
 
-# initialize DB tables on app start
+# Initialize DB on start
 init_db()
 
 @app.route("/")
 def home():
-    return "Pharma App Running Successfully!"
+    return render_template("login_page.html")
+
+@app.route("/login_validation", methods=["POST"])
+def login_validation():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    # Check user in database
+    user = cursor.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username, password)
+    ).fetchone()
+
+    if user:
+        return "LOGIN SUCCESSFUL!"
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
